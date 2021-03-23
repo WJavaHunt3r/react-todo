ed5e615972c0e993ab09e1af2e02f9bc08e567c8
WJavaHunt3r <you@example.com>
2021-03-23 20:09:00 +0100

    Final basic app with filter

--- my-todo/src/App.js
+++ my-todo/src/App.js
@@ -6,6 +6,14 @@ import FilterButton from "./components/FilterButton";
 import React, {useState} from "react";
 import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
   function toggleTaskCompleted(id) {
@@ -32,7 +40,18 @@ function App(props) {
     });
     setTasks(editedTasks);
   }
-  const taskList = tasks.map(task => (
+
+  const [filter, setFilter] = useState('All');
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton 
+      key={name} 
+      name={name}
+      isPressed={name===filter}
+      setFilter={setFilter}
+    />
+  ));
+  const taskList = tasks.filter(FILTER_MAP[filter])
+  .map(task => (
     <Todo 
       id={task.id}
       name = {task.name}
@@ -46,15 +65,14 @@ function App(props) {
   );
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
+  
 
   return (
     <div className="todoapp stack-large">
       <h1>My Todo</h1>
       <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">
         {headingText}
