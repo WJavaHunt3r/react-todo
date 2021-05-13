7f3386d9641fb6706356491ea949914b00e536c3
WJavaHunt3r <wagnerandre18@gmail.com>
2021-05-13 16:55:32 +0200

    deleting bin and obj

--- raect-todo/ReactTodo.Data/Migrations/20210513144905_init.cs
+++ raect-todo/ReactTodo.Data/Migrations/20210513144905_init.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace ReactTodo.Data.Migrations
 {
-    public partial class MyMigration : Migration
+    public partial class init : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -13,7 +13,7 @@ namespace ReactTodo.Data.Migrations
                 {
                     Id = table.Column<long>(type: "bigint", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                    Name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true)
                 },
                 constraints: table =>
                 {
@@ -26,7 +26,7 @@ namespace ReactTodo.Data.Migrations
                 {
                     Id = table.Column<long>(type: "bigint", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
                     Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     Priority = table.Column<int>(type: "int", nullable: false),
                     BoardId = table.Column<long>(type: "bigint", nullable: false),
@@ -41,6 +41,28 @@ namespace ReactTodo.Data.Migrations
                         principalTable: "Boards",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Boards",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1L, "Todo" },
+                    { 2L, "Active" },
+                    { 3L, "Blocked" },
+                    { 4L, "Completed" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "TodoItems",
+                columns: new[] { "Id", "BoardId", "DeadLine", "Description", "Priority", "Title" },
+                values: new object[,]
+                {
+                    { 1L, 1L, new DateTime(2021, 4, 24, 0, 0, 0, 0, DateTimeKind.Unspecified), "My fist todo", 1, "Todo #1" },
+                    { 2L, 1L, new DateTime(2021, 4, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), "My second todo", 2, "Todo #2" },
+                    { 3L, 1L, new DateTime(2021, 4, 26, 0, 0, 0, 0, DateTimeKind.Unspecified), "My third todo", 3, "Todo #3" },
+                    { 4L, 1L, new DateTime(2021, 4, 27, 0, 0, 0, 0, DateTimeKind.Unspecified), "My fourth todo", 4, "Todo #4" }
                 });
 
             migrationBuilder.CreateIndex(
